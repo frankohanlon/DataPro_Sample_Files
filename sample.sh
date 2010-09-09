bdd191f66c773af88a7a530118f7ef67f18d5130
Bob <rcbusey@alaska.edu>
2010-09-09 15:24:31 -0800

    There should be enough files here correctly configured to get DataPro and utilities running on the OS of your choice.  Of course, modify the paths in the key files and script files before running it all on your machine... After downloading everything and changing paths all over try running sample.sh or win_sample.bat --Bob

---
+++ sample.sh
@@ -0,0 +1,4 @@
+python /Users/bob/working_files/bin/py_datapro/DataPro/datapro.py /Users/bob/working_files/bin/py_datapro/DataPro_Sample_Files/sample_array_key.txt
+python /Users/bob/working_files/bin/py_datapro/DataPro/datapro.py /Users/bob/working_files/bin/py_datapro/DataPro_Sample_Files/sample_table_key.txt
+python /Users/bob/working_files/bin/py_datapro/DataPro/glom_together.py /Users/bob/working_files/bin/py_datapro/DataPro_Sample_Files/glom_sample_wind.txt
+python /Users/bob/working_files/bin/py_datapro/DataPro/daily_averages.py /Users/bob/working_files/bin/py_datapro/DataPro_Sample_Files/outputs/array_Temperature.csv
---
+++ win_sample.bat
@@ -0,0 +1,4 @@
+c:\python25\python.exe c:\data\DataPro\datapro.py c:\data\DataPro_Sample_Files\win_sample_array_key.txt
+c:\python25\python.exe c:\data\DataPro\datapro.py c:\data\DataPro_Sample_Files\win_sample_table_key.txt
+c:\python25\python.exe c:\data\DataPro\glom_together.py c:\data\DataPro_Sample_Files\win_glom_sample_wind.txt
+c:\python25\python.exe c:\data\DataPro\daily_averages.py C:\data\DataPro_Sample_Files\outputs\array_Temperature.csv
